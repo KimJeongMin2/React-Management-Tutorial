ef3c213e52a2fa6666c0679d50c0838cca772b90
kimjeongmin <kij9451@naver.com>
2023-07-05 16:14:50 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"></img>
-      <h1>Let's develop management system!</h1>
-    </div>
-  );
+const customers = [{
+  'id' :1,
+  'image': 'https://picsum.photos/64?random=1',
+  'name': '홍길동',
+  'birthday' : '961222',
+  'gender':'남자',
+  'job':'대학생'
+},
+{
+  'id' :2,
+  'image': 'https://picsum.photos/64?random=2',
+  'name': '나동빈',
+  'birthday' : '975120',
+  'gender':'여자',
+  'job':'프로그래머'
+},
+{
+  'id':3,
+  'image': 'https://picsum.photos/64?random=3',
+  'name': '이순신',
+  'birthday' : '941234',
+  'gender':'남자',
+  'job':'디자이너'
+}]
+class App extends Component{
+  render(){
+    return (
+      <div>
+        {
+          customers.map(c=>{
+            return (
+              <Customer
+              key={c.id}
+               id={c.id}
+               image={c.image}
+               name={c.name}
+               gender={c.gender}
+               job={c.job}
+              />
+            )
+          })
+        }
+      
+
+      </div>  
+    );
+  }
 }
 
 export default App;
